0a396607f2544cfec8ef9851dfc1503ecb944792
Adam <adamh93@verizon.net>
2019-06-03 14:18:42 -0400

    done: Remove tenary statements for text and icon in SeasonDisplay comp, Add a configObject called seasonConfig that returns text and iconName for whether it is summer or winter, In SeasonDisplay component create a const for text and iconName that retrieves its values from seasonConfig const that has parameter of season, Refactor icon in return method in render to use iconName instead of icon and put a class massive to make icons size large

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const seasonConfig = {
+  summer: {
+    text: 'Lets hit the beach',
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'Burr, its is chilly',
+    iconName: 'snowflake'
+  }
+};
 
 const getSeason = (lat, month) => {
   if(month > 2 && month < 9){
@@ -12,15 +22,13 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const text = season === 'winter' ? 'Burr, its is chilly' : 'Lets hit the beach';
-
-  const icon = season === 'winter' ? 'snowflake' : 'sun';
+  const {text, iconName} = seasonConfig[season];  // {text, iconName}
 
   return(
     <div>
-      <i className={`${icon} icon`} />
+      <i className={`massive ${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${icon} icon`} />
+      <i className={`massive ${iconName} icon`} />
     </div>
   );
 };
